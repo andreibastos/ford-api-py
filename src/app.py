652b9452b3397890d55667b4b312dd0dcdd65b45
Andrei Bastos <andreibastos@outlook.com>
2018-02-22 16:58:14 -0300

    Adicionado funções do usuário

--- src/app.py
+++ src/app.py
@@ -13,6 +13,8 @@ from flask import Flask, jsonify, abort, request, url_for,  make_response
 from flask_httpauth import HTTPBasicAuth
 import mongoengine
 
+import datetime, json
+
 # my modules
 from dao import DAODocument
 
@@ -29,36 +31,81 @@ path_default_user = path_default+"user/"
 mongoengine.connect('ford')
 
 class User(mongoengine.Document):
-    email = mongoengine.StringField(required=True)
+    email = mongoengine.StringField(required=True, unique=True)
     password = mongoengine.StringField(required=True)
     first_name = mongoengine.StringField(required=True, max_length=50)
     last_name = mongoengine.StringField(max_length=50)
+    
+    def directory_create(self,name, source_id=None):        
+        source_document = None
+        if source_id:            
+            source_document = Directory.objects.get(id=source_id)            
+            if not source_document:
+                raise InvalidUsage('not exist directory with id={0}'.format(source_id))                        
+        else:              
+            try:           
+                source_document = Directory.objects.get(source_document=None)                
+            except Directory.DoesNotExist:                
+                source_document = Directory(name='root', description='root directory', source_document=None, date_created=datetime.datetime.utcnow(), date_modified=datetime.datetime.utcnow(), author=self )
+                source_document.save()
+
+        documents = Document.objects(source_document = source_document.id)                
+        for document in documents:            
+            if str(document.name).lower() == str(name).lower():
+                raise InvalidUsage('exist directory with name={0}'.format(name))                        
+        
+            
+
+
+        return Directory(name=name, author = self, source_document=source_document, date_created=datetime.datetime.utcnow(), date_modified=datetime.datetime.utcnow())
+
+    def get_documents(self, name_or_id):
+        source_document = None
+        try:           
+            source_document = Document.objects.get(name=name_or_id)
+        except Document.DoesNotExist:
+            try:
+                source_document = Document.objects.get(id=name_or_id)                                
+            except:
+                raise 
+        
+        if source_document:
+            documents = Document.objects.get(source_document=source_document)                
+            return documents
+        
+    def delete_document(self,id):
+        document = Document.objects.get(id=id, author = self)
+        if document:
+            document[0].delete()
+
+    def delete_documents(self, ids):
+        documents = Document.objects.get(author = self)
+        for document in documents:
+            document.delete()
+
 
 class Document(mongoengine.Document):
     name = mongoengine.StringField(required=True)    
-    is_favorited = mongoengine.BooleanField()
+    is_favorited = mongoengine.BooleanField(default=False)
     author = mongoengine.ReferenceField(User, required=True, reverse_delete_rule=mongoengine.CASCADE)
     date_created = mongoengine.DateTimeField()
     date_modified = mongoengine.DateTimeField()
-    space_disk =  mongoengine.IntField()
-    described = mongoengine.StringField()
-    tags = mongoengine.ListField(mongoengine.StringField(max_length=30))
-
+    space_disk =  mongoengine.IntField(default=0)
+    description = mongoengine.StringField()
+    tags = mongoengine.ListField(mongoengine.StringField(max_length=50),default=None)    
     meta = {'allow_inheritance': True}
 
 
 class Directory(Document):
-    source_document = mongoengine.ReferenceField(Document, reverse_delete_rule=mongoengine.CASCADE)    
+    source_document = mongoengine.ReferenceField(Document, reverse_delete_rule=mongoengine.CASCADE)        
     
     
 class File(Document):
     source_document = mongoengine.ReferenceField(Document, reverse_delete_rule=mongoengine.CASCADE)
     
-    
 class Process(Document):
     source_document = mongoengine.ReferenceField(Document, reverse_delete_rule=mongoengine.CASCADE)
     
-
 class InvalidUsage(Exception):
     status_code = 400
 
@@ -84,64 +131,45 @@ def handle_invalid_usage(error):
 
 @app.route(path_default_documents, methods=['GET'])
 @auth.login_required
-def get_documents(): 
-    documents = []
+def get_document(name_or_id):    
     try:
-        return jsonify({'documents': documents})        
-    except Exception as identifier: 
-        raise InvalidUsage(identifier.message, status_code=410)       
-
-@app.route(path_default_documents+'<int:id_document>', methods=['GET'])
-@auth.login_required
-def get_document(id_document):
-    document = ['andrei']
-    if len(document) == 0:
-        abort(404)
-    return jsonify({'document': document[0]})
+        print request
+        documents = auth.user.get_documents(name_or_id)                        
+        if not documents:
+            abort(404)
+        return jsonify({'documents': json.loads(documents.to_json(ensure_ascii=False))})
+    except Exception as identifier:
+        print identifier        
+        InvalidUsage(identifier.message)
+   
 
 @app.route(path_default_documents, methods=['POST'])
 @auth.login_required
-def create_document():    
-    def create_directory(request):    
-        source_id = request.get('source_id')
-        if source_id :            
-            source_document = Directory.objects.get(id=source_id)
-            if not source_document:
-                raise InvalidUsage('not exist id={0}'.format(source_id))
-        else:
-            source_document = Directory(name='root', author = auth.user, source_document=None)
-
-        print source_document.to_json()    
-        source_document.save()
-        
-        
-        return Directory( name= request['name'],            
-            source_document = source_document,
-            described= request.get('description'),
-            is_favorited=request.get('is_favorited') or  False,
-            author = auth.user
-        )
-
+def create_document():
     if not request.json or not 'type' in request.json:
-        abort(400)       
+        raise InvalidUsage('not exist type in post.')
 
     if request.json['type'] == 'directory':
-        try:            
-            directory = create_directory(request.json)                        
-            directory.save()            
+        try:
+            name= request.json.get('name')
+            source_id = request.json.get('source_id')            
+            description = request.json.get('description')
+            is_favorited = request.json.get('is_favorited') or False
+            directory = auth.user.directory_create(name=name,source_id=source_id)                                    
+            directory.is_favorited = is_favorited
+            directory.description = description
+            directory.save()
+            return jsonify({'result': json.loads(directory.to_json(ensure_ascii=False))}), 201
         except Exception as identifier:
             raise InvalidUsage(identifier.message, status_code=410)       
-    
-    
-    return jsonify({'result': directory.to_json() }), 201
 
-@app.route(path_default_documents+'<int:id_document>', methods=['PUT'])
+@app.route(path_default_documents+'<id>', methods=['PUT'])
 @auth.login_required
 def update_document(id_document):    
     pass
     # return jsonify({'document': document[0]})
 
-@app.route(path_default_documents+'<int:id_document>', methods=['DELETE'])
+@app.route(path_default_documents+'<id>', methods=['DELETE'])
 @auth.login_required
 def delete_document(id_document):
     pass
